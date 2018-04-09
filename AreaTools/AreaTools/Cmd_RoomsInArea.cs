20196c30fdce9bffb7ca3e37737c1dfb3218661a
rm <rm@LAPTOP-64B1EHF0>
2018-04-08 21:40:31 -0400

    Added command and utils

---
+++ AreaTools/AreaTools/Cmd_RoomsInArea.cs
@@ -0,0 +1,62 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AreaTools.DataModels;
+using AreaTools.Utils;
+
+namespace AreaTools
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Cmd_RoomsInArea : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            ElementCategoryFilter roomFilter = new ElementCategoryFilter(BuiltInCategory.OST_Rooms);
+            var rooms = new FilteredElementCollector(doc, uidoc.ActiveView.Id)
+                .WherePasses(roomFilter)
+                .WhereElementIsNotElementType()
+                .Cast<SpatialElement>();
+
+            AreaFilter areaFilter = new AreaFilter();
+            var areaObjects = new FilteredElementCollector(doc, uidoc.ActiveView.Id)
+                .WherePasses(areaFilter)
+                .WhereElementIsNotElementType()
+                .Cast<Area>()
+                .Select(x => new AreaObject(x));
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (AreaObject areaobject in areaObjects)
+            {
+                List<SpatialElement> roomObjects = new List<SpatialElement>();
+                foreach (SpatialElement roomElement in rooms)
+                {
+                    LocationPoint lp = roomElement.Location as LocationPoint;
+                    if (areaobject.Area.AreaContains(lp.Point))
+                    {
+                        roomObjects.Add(roomElement);
+                        sb.AppendLine(areaobject.Area.Name + " ;" + roomElement.Name);
+                    }
+                }
+                areaobject.Rooms = roomObjects;
+                sb.AppendLine(areaobject.Area.Name + " ; Room Count:" + areaobject.Rooms.Count);
+            }
+
+            TaskDialog.Show("Area Objects", sb.ToString());
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ AreaTools/AreaTools/DataModels/AreaObject.cs
@@ -0,0 +1,46 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTools.DataModels
+{
+    class AreaObject
+    {
+        private Area _area;
+        private IList<CurveLoop> _curveLoopList;
+        private List<SpatialElement> _rooms;
+
+        public AreaObject(Area area)
+        {
+            _area = area;
+            _rooms = new List<SpatialElement>();
+
+            SpatialElementBoundaryOptions opt = new SpatialElementBoundaryOptions();
+            opt.StoreFreeBoundaryFaces = true;
+            opt.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Center;
+
+            _curveLoopList = new List<CurveLoop>();
+            var loopList = area.GetBoundarySegments(opt);
+            foreach (var boundarySegList in loopList)
+            {
+                CurveLoop curves = new CurveLoop();
+                foreach (var boundarySeg in boundarySegList)
+                {
+                    curves.Append(boundarySeg.GetCurve());
+                }
+                _curveLoopList.Add(curves);
+            }
+        }
+
+        public Area Area { get { return _area; } }
+        public IList<CurveLoop> CurveLoopList { get { return _curveLoopList; } }
+        public List<SpatialElement> Rooms
+        {
+            get { return _rooms; }
+            set { _rooms = value; }
+        }
+    }
+}
